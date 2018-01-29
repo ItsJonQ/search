7fcb3da73e4fd0afc9bf248142f7bd3ed79459b4
ItsJonQ <itsjonq@gmail.com>
2018-01-29 11:08:53 -0500

    Update search to handle URL as well as search queries

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const SearchInputNode = document.querySelector('.SearchInput')
+const searchEngineUrl = 'https://duckduckgo.com/?q='
+const doSearch = (event) => {
+  event.preventDefault()
+  const value = SearchInputNode.value
+  if (
+    value.indexOf('http:') >= 0 ||
+    value.indexOf('https:') >= 0 ||
+    value.indexOf('localhost:') >= 0
+  ) {
+    return window.location.href = value
+  } else {
+    return window.location.href = `${searchEngineUrl}${value}`
+  }
+}
