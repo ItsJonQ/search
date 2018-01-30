701ceca5df745dc3235ec6efa52efd46488162c2
ItsJonQ <itsjonq@gmail.com>
2018-01-29 20:05:06 -0500

    Fix logic

--- scripts.js
+++ scripts.js
@@ -3,7 +3,15 @@ const searchEngineUrl = 'https://duckduckgo.com/?q='
 const doSearch = (event) => {
   event.preventDefault()
   const value = SearchInputNode.value
-  if (value.indexOf('localhost:') === 0) {
+  if (
+    value.indexOf('http:') >= 0 ||
+    value.indexOf('https:') >= 0 ||
+    value.indexOf('localhost:') >= 0
+  ) {
+    return window.location.href = value
+  } else if (
+    value.indexOf('localhost:') === 0
+  ) {
     return window.location.href = `http://${value}`
   } else if (
     value.indexOf('.com') >= 0 ||
@@ -11,12 +19,6 @@ const doSearch = (event) => {
     value.indexOf('.io') >= 0
   ) {
     return window.location.href = `http://${value}`
-  } else if (
-    value.indexOf('http:') >= 0 ||
-    value.indexOf('https:') >= 0 ||
-    value.indexOf('localhost:') >= 0
-  ) {
-    return window.location.href = value
   } else {
     return window.location.href = `${searchEngineUrl}${value}`
   }
